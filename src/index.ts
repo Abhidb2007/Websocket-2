93852b8659d8104f6def97bc7b73b33678d7c6e2
Abhidb2007 <abhishekdb2210@gmail.com>
2025-10-29 18:16:10 +0530

    code

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ wss.on("connection",(socket)=>{
     userCount=userCount+1;
     console.log("user connected #"+userCount);
     socket.on("message",(message)=>{
-        console.log("message received"+ message.toString())
-     })
+        console.log("message received"+message.toString());
+        socket.send(message.toString()+":sent from the server");
+    });
 });
